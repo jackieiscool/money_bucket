e36b658d3e2ee09abe637866f17c3696d8b32b26
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-12-11 22:43:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoneyBucket::Application.config.secret_key_base = 'cd3480220f35ea1a11f7a28008f2d73bda8170cbffabac37cdd4705cb52f8b4fc51a7245450603bc1dd01c229c32ad7640e6f160249c806f70adf2a370e1361a'
